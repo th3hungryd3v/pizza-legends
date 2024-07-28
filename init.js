62f022e59840e7d7f2016e553bb6b2218b52adfe
th3hungryd3v <th3hungryd3v@gmail.com>
2024-07-27 18:04:17 -0700

    Added more notes

--- init.js
+++ init.js
@@ -1,8 +1,10 @@
 // Write a function that executes itself
 (function () {
   // console.log("It's working!")
+  // Create a new instance
   const overworld = new Overworld({
     element: document.querySelector(".game-container")
   })
+  // Call the function available to the instance
   overworld.init();
 })();
