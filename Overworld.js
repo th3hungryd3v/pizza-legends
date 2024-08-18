9452d22deb7caa55f2c3231b621dc688d5d7e85c
th3hungryd3v <th3hungryd3v@gmail.com>
2024-08-18 08:24:58 -0700

    Canvas is blank, but it's because we're not actually doing any of our 'drawing' code anymore

--- Overworld.js
+++ Overworld.js
@@ -4,31 +4,22 @@ class Overworld {
     this.element = config.element; // Pass in an element for the overworld to operate on
     this.canvas = this.element.querySelector(".game-canvas"); // Grab the canvas tag and save it to this.canvas
     this.ctx = this.canvas.getContext("2d"); // Attach a reference to ctx, and give it access to 2d canvas drawing tools
+    this.map = null;
+  }
+
+  // Game Loop
+  startGameLoop() {
+    const step = () => {
+      requestAnimationFrame(() => {
+        step();
+      })
+    }
+    step();
   }
 
   init() {
-    // console.log("Hello from the Overworld", this)
-    const image = new Image();
-    image.onload = () => {
-      // (Where to draw pixels from, x coordiante, y coordinate)
-      this.ctx.drawImage(image, 0, 0); //
-    };
-    image.src = "/images/maps/DemoLower.png";
-
-    // Place some Game Objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    });
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: "images/characters/people/npc1.png",
-    });
-    // Make sure the Image is loaded then draw sprites to the canvas
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom)
+    this.startGameLoop();
+    
   }
 }
